de8d71cc552c52995fa093ac7c9732370e359a6f
ekavajh <javid.khasizada@ericsson.com>
2023-03-26 22:44:32 +0200

    Update list of weapons

--- weapons.js
+++ weapons.js
@@ -1,5 +1,107 @@
 const weapons = [
     {
+        "id": "bb",
+        "name": "Star Sickle",
+        "dice": [
+            {
+                "count": 1,
+                "type": 6,
+                "damage": "default"
+            },
+            {
+                "count": 1,
+                "type": 4,
+                "damage": "radiant"
+            },
+            {
+                "count": 1,
+                "type": 4,
+                "damage": "radiant"
+            },
+        ],
+        "modifier": 1
+    },
+    {
+        "id": "ff",
+        "name": "Maul",
+        "dice": [
+            {
+                "count": 1,
+                "type": 6,
+                "damage": "default"
+            },
+            {
+                "count": 1,
+                "type": 6,
+                "damage": "default"
+            },
+            {
+                "count": 1,
+                "type": 4,
+                "damage": "fire"
+            },
+        ],
+        "modifier": 2
+    },
+    {
+        "id": "gg",
+        "name": "Spear",
+        "dice": [
+            {
+                "count": 1,
+                "type": 8,
+                "damage": "default"
+            },
+        ],
+        "modifier": 2
+    },
+    {
+        "id": "cc",
+        "name": "Star Razor",
+        "dice": [
+            {
+                "count": 1,
+                "type": 8,
+                "damage": "default"
+            },
+        ],
+        "modifier": 2
+    },
+    {
+        "id": "dd",
+        "name": "Bite",
+        "dice": [
+            {
+                "count": 1,
+                "type": 10,
+                "damage": "default"
+            },
+        ],
+        "modifier": 2
+    },
+    {
+        "id": "ee",
+        "name": "Claw",
+        "dice": [
+            {
+                "count": 1,
+                "type": 8,
+                "damage": "default"
+            },
+            {
+                "count": 1,
+                "type": 8,
+                "damage": "default"
+            },
+            {
+                "count": 1,
+                "type": 6,
+                "damage": "default"
+            },
+        ],
+        "modifier": 0
+    },
+    {
         "id": "aa",
         "name": "Greatsword of Artorius",
         "dice": [
@@ -15,47 +117,13 @@ const weapons = [
             },
             {
                 "count": 1,
-                "type": 4,
+                "type": 6,
                 "damage": "force"
             },
             {
                 "count": 1,
                 "type": 6,
                 "damage": "radiant"
-            },
-        ],
-        "modifier": 0
-    },
-    {
-        "id": "bb",
-        "name": "Star Sickle",
-        "dice": [
-            {
-                "count": 1,
-                "type": 6,
-                "damage": "default"
-            },
-            {
-                "count": 1,
-                "type": 4,
-                "damage": "default"
-            },
-            {
-                "count": 1,
-                "type": 4,
-                "damage": "force"
-            },
-        ],
-        "modifier": 1
-    },
-    {
-        "id": "cc",
-        "name": "Star Razor",
-        "dice": [
-            {
-                "count": 1,
-                "type": 8,
-                "damage": "default"
             },
         ],
         "modifier": 2
