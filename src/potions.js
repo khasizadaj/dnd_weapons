60a01223f495e17108fcfcaa7f5e1ff3c98f6ab6
ekavajh <javid.khasizada@ericsson.com>
2023-03-27 20:30:30 +0200

    Add logic for random potion selection

---
+++ src/potions.js
@@ -0,0 +1,19 @@
+const POTIONS = [
+    {
+        "title": "Potion of Diminution 1",
+        "description": "When you drink this potion, you gain the 'reduce' effect of the Enlarge/Reduce spell for 1d4 hours (no concentration required). The red in the potion's liquid continuously contracts to a tiny bead and then expands to color the clear liquid around it. Shaking the bottle fails to interrupt this process.",
+        "source": "Dungeon Master's Guide"
+    },
+    {
+        "title": "Potion of Diminution 2",
+        "description": "When you drink this potion, you gain the 'reduce' effect of the Enlarge/Reduce spell for 1d4 hours (no concentration required). The red in the potion's liquid continuously contracts to a tiny bead and then expands to color the clear liquid around it. Shaking the bottle fails to interrupt this process.",
+        "source": "Dungeon Master's Guide"
+    },
+    {
+        "title": "Potion of Diminution 3",
+        "description": "When you drink this potion, you gain the 'reduce' effect of the Enlarge/Reduce spell for 1d4 hours (no concentration required). The red in the potion's liquid continuously contracts to a tiny bead and then expands to color the clear liquid around it. Shaking the bottle fails to interrupt this process.",
+        "source": "Dungeon Master's Guide"
+    },
+]
+
+export { POTIONS };
--- src/random_potion.js
+++ src/random_potion.js
@@ -1,33 +1,57 @@
-import { roll } from "./utils.js";
+import { POTIONS } from "./potions.js";
+import { ElementParams, getElement, roll } from "./utils.js";
 
 const NUMBER_OF_POTIONS = 3;
 
-const POTIONS = [
-    {
-        "title": "Potion of Diminution 1",
-        "description":"When you drink this potion, you gain the 'reduce' effect of the Enlarge/Reduce spell for 1d4 hours (no concentration required). The red in the potion's liquid continuously contracts to a tiny bead and then expands to color the clear liquid around it. Shaking the bottle fails to interrupt this process.",
-        "source":"Dungeon Master's Guide"
-    },
-    {
-        "title": "Potion of Diminution 2",
-        "description":"When you drink this potion, you gain the 'reduce' effect of the Enlarge/Reduce spell for 1d4 hours (no concentration required). The red in the potion's liquid continuously contracts to a tiny bead and then expands to color the clear liquid around it. Shaking the bottle fails to interrupt this process.",
-        "source":"Dungeon Master's Guide"
-    },
-    {
-        "title": "Potion of Diminution 3",
-        "description":"When you drink this potion, you gain the 'reduce' effect of the Enlarge/Reduce spell for 1d4 hours (no concentration required). The red in the potion's liquid continuously contracts to a tiny bead and then expands to color the clear liquid around it. Shaking the bottle fails to interrupt this process.",
-        "source":"Dungeon Master's Guide"
-    },
-]
-
 const generateButton = document.querySelector("#generate");
+const potionGeneratorSection = document.querySelector(".potion-generator");
+
 
 generateButton.addEventListener("click", () => {
     generateRandomPotion();
 })
 
+const getPotionElement = (potion) => {
+    let potionElement = getElement(
+        new ElementParams(
+            { "type": "div", "classes": "potion", "id": "selected" }
+        )
+    );
+
+    let title = getElement(
+        new ElementParams(
+            { "type": "h3", "classes": "potion-title", "content": potion.title }
+        )
+    )
+
+    let description = getElement(
+        new ElementParams(
+            { "type": "p", "classes": "potion-description", "content": potion.description }
+        )
+    )
+
+    let source = getElement(
+        new ElementParams(
+            { "type": "p", "classes": "potion-source", "content": `<b>Source: </b>${potion.source}`, contentType: "html" }
+        )
+    )
+
+    potionElement.appendChild(title);
+    potionElement.appendChild(description);
+    potionElement.appendChild(source);
+    return potionElement;
+}
+
 const generateRandomPotion = () => {
     let randomPotionNumber = roll(NUMBER_OF_POTIONS);
-    let potion = POTIONS[randomPotionNumber - 1];
-    console.log(potion)
+    let selectedPotion = POTIONS[randomPotionNumber - 1];
+
+    let potionElement = getPotionElement(selectedPotion);
+
+    if (document.querySelector(".potion")) {
+        potionGeneratorSection.removeChild(document.querySelector(".potion"));
+        potionGeneratorSection.appendChild(potionElement);
+    } else {
+        potionGeneratorSection.appendChild(potionElement);
+    }
 } 
