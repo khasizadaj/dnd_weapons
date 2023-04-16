8cf267bedcb211a69d2344f370ceb3b7c400f7d3
ekavajh <javid.khasizada@ericsson.com>
2023-04-16 15:25:59 +0200

    Add cryptographical random number generation

--- die.js
+++ die.js
@@ -1,7 +1,9 @@
 export const roll = (max) => {
-    let min = 1;
-    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
-};
+    var randomBytes = new Uint8Array(4);
+    crypto.getRandomValues(randomBytes);
+    var randomInt = (randomBytes[0] << 24) | (randomBytes[1] << 16) | (randomBytes[2] << 8) | randomBytes[3];
+    return Math.floor(Math.abs(randomInt) / 0x7fffffff * max) + 1;
+}
 
 export const rollD20 = () => {
     return roll(20);
